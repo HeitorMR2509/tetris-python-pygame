a99f90e2f0180ff350bf27e449facddd9fcc9efa
HeitorMR2509 <heitor2509@outlook.com>
2025-01-23 11:40:12 -0300

    Removidos comentários desnecessários & variáveis renomeadas

--- main.py
+++ main.py
@@ -1,44 +1,38 @@
-# Permite colocar todas as variáveis globais e 
-# bibliotecas importadas em outro arquivo.
+# Configurações e bibliotecas importadas
 from config import *
 
 def main() -> None:
-    # Configuração geral
-    # Inicia o Pygame
     pygame.init()
 
-    # Variável de continuidade
     rodando = True
 
     # Cria uma superfície da janela
-    janela_superficie = pygame.display.set_mode(
+    janela = pygame.display.set_mode(
         (LARGURA_JANELA, COMPRIMENTO_JANELA)
     )
 
-    # Muda título da janela
     pygame.display.set_caption("Tetris")
 
     # Criação de um "relógio" para FPS e tempo
     relogio = pygame.time.Clock()
 
-    # Repetição do jogo
     while rodando:
 
         # Muda cor de fundo da janela
-        janela_superficie.fill(CINZA)
+        janela.fill(CINZA)
 
-        # Atualiza a janela
         pygame.display.update()
 
         # Ritmo de atualização. None=máximo possível e parâmetro 
         # é o fps
-        relogio.tick()
+        relogio.tick(60)
 
-        # Lida com eventos do Pygame
         for event in pygame.event.get():
-            # Evento de fechar janela
             if event.type == pygame.QUIT:
                 rodando = False
+
+    # Limpa a "tralha" da biblioteca/desinicializa
+    pygame.quit()
 
 
 if __name__ == "__main__":
