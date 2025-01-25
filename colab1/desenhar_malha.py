07156f44b09073fda31372218354c1f697da5c12
Natano10 <fsb.nata@gmail.com>
2025-01-25 16:12:38 -0300

    Código original copiado

--- colab1/desenhar_malha.py
+++ colab1/desenhar_malha.py
@@ -1,7 +1,32 @@
-def a() -> None:
-    print("hello")
+from settings import *
+class Game:
+    def __Init __(self):
 
-    print("qeohgiowerhiogj")
+        # general
+        self.Surfade = pygame.Surface((GAME WIDTH, GAME HEIGHT))
+        self.display_surface = pygame.display.get_surface()
+        self.rect = self.surface.get_rect(topleft = (PADDING, PADDING)
+        
+        # lines
+        Self.line_surface = self.surface.copy()
+        self.line_surface.fill((0,255,0))
+        self.line_surface.set_colorkey((9,255,0))
+        self.line_surface.set_alpha(126)
+    
+    def draw_grid(self):
+        for col in range(1, COLUMNS):
+            x = col * CELL_SIZE
+            pygame.draw.line(self.line_surface, LINE_COLOR, (x,0), (x,self.surface.get_width(), 1))
 
+        for row in range(1, ROWS):
+            y = row * CELL_SIZE
+            pygame.draw.line(self.line_surface, LINE_COLOR, (0,y), (self.surface.get_width(),y))
 
+    def run(self):
+
+        # drawing
+        self.surface.fill(GRAY)
+
+        self.draw_grid()
+        self.display_surface.blit(self.surface, (PADDING,PADDING))
     
